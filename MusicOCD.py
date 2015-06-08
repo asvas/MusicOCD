aed215a1235ee3cbc80d91ba547ae3dec648b5d2
asvas <asvas@live.com>
2015-06-08 09:16:44 +0300

    Update MusicOCD.py

--- MusicOCD.py
+++ MusicOCD.py
@@ -1,1 +1,33 @@
-#First try.
+#import eyed3
+#help(eyed3)
+#tag = eyed3.tag()
+#tag.link("C:/Users/Vasil/Desktop/Test.mp3")
+#print(tag.getArtist())
+#print(tag.getAlbum())
+#print(tag.getTitle())
+
+
+def opener():
+	#Opening and generating informational data of files.
+	#Multiple libreries could be chosen.
+	#Library to be chosen.
+	pass
+
+def search():
+	#Locating and extranction of information.
+	#Multiple search engines might be added.
+	#APIs to be chosen.
+	pass
+
+def evaluate():
+	#Evaluation of the extracted data from the search.
+	#Multiple sources comparison migh be used.
+	pass
+
+def editor():
+	#Overright or add the evaluated information.
+	pass
+
+def backup_infor():
+	#To make a readable text format of all administrated changes.
+	pass
